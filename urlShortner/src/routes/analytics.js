8f36c765997c314c6de7d00610fdc67066772fb9
Sayan Singh <157497677+singhsayan@users.noreply.github.com>
2025-02-20 11:26:45 +0530

    Delete urlShortner directory

--- urlShortner/src/routes/analytics.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-const { getAnalytics } = require("../controllers/analyticsController");
-
-router.get("/:shortUrl", getAnalytics);
-
-module.exports = router;
--- urlShortner/src/routes/urlRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-const { shortenUrl, redirectUrl } = require("../controllers/urlController");
-
-router.post("/shorten", shortenUrl);
-router.get("/:shortUrl", redirectUrl);
-
-module.exports = router;
