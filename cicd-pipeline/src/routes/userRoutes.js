ecc9a339d0bb8e383616726d14ed3dbf8bf0433d
Sayan Singh <157497677+singhsayan@users.noreply.github.com>
2025-02-20 12:30:12 +0530

    Update userRoutes.js

--- cicd-pipeline/src/routes/userRoutes.js
+++ cicd-pipeline/src/routes/userRoutes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const userControllers = require('../controllers/userControllers'); // Corrected import
+const userControllers = require('../controllers/userControllers'); 
 
-// Route to get all users
 router.get('/', userControllers.getUsers);
 
-// Route to add a new user
 router.post('/', userControllers.addUser);
 
 module.exports = router;
