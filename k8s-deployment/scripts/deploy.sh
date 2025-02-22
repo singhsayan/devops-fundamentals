f002498c8224653d967d62609cc6fb7125c3033e
Sayan Singh <157497677+singhsayan@users.noreply.github.com>
2025-02-22 10:41:58 +0530

    Update deploy.sh

--- k8s-deployment/scripts/deploy.sh
+++ k8s-deployment/scripts/deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Applying Kubernetes Secrets & ConfigMaps..."
+echo "Applying Kubernetes Secrets & ConfigMaps"
 kubectl apply -f manifests/k8s-secret.yaml
 kubectl apply -f manifests/k8s-configmap.yaml
 
-echo "Deploying PostgreSQL..."
+echo "Deploying PostgreSQL"
 kubectl apply -f manifests/postgres-deployment.yaml
 kubectl apply -f manifests/postgres-service.yaml
 
-echo "Deploying Node.js App..."
+echo "Deploying Node.js App"
 kubectl apply -f manifests/app-deployment.yaml
 kubectl apply -f manifests/app-service.yaml
 
-echo "Deployment complete!"
+echo "Deployment complete"
 kubectl get pods
 kubectl get svc
