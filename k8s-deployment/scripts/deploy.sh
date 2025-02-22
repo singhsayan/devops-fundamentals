56f3d56d0063ef98fcba5bb860c21282caccc464
Sayan Singh <157497677+singhsayan@users.noreply.github.com>
2025-02-22 09:46:56 +0530

    Add files via upload

---
+++ k8s-deployment/scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Applying Kubernetes Secrets & ConfigMaps..."
+kubectl apply -f manifests/k8s-secret.yaml
+kubectl apply -f manifests/k8s-configmap.yaml
+
+echo "Deploying PostgreSQL..."
+kubectl apply -f manifests/postgres-deployment.yaml
+kubectl apply -f manifests/postgres-service.yaml
+
+echo "Deploying Node.js App..."
+kubectl apply -f manifests/app-deployment.yaml
+kubectl apply -f manifests/app-service.yaml
+
+echo "Deployment complete!"
+kubectl get pods
+kubectl get svc
