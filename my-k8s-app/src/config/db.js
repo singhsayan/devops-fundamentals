3db8361faeee35c2a59bdac6b2da7c2fec0a6dda
Sayan Singh <157497677+singhsayan@users.noreply.github.com>
2025-02-25 14:33:22 +0530

    Update db.js

--- my-k8s-app/src/config/db.js
+++ my-k8s-app/src/config/db.js
@@ -5,16 +5,16 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.POSTGRES_URI, {
   dialect: "postgres",
-  logging: false // Disable logging in production
+  logging: false 
 });
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log("✅ PostgreSQL Database Connected");
+    console.log("PostgreSQL Database Connected");
   } catch (error) {
-    console.error("❌ Database Connection Error:", error);
-    process.exit(1); // Exit process with failure
+    console.error("Database Connection Error:", error);
+    process.exit(1); 
   }
 };
 
