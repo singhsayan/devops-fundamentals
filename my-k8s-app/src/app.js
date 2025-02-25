f8df6b2ae4055b55e64037ab452bae3af0c28411
Sayan Singh <157497677+singhsayan@users.noreply.github.com>
2025-02-25 14:34:00 +0530

    Update app.js

--- my-k8s-app/src/app.js
+++ my-k8s-app/src/app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import { connectDB, sequelize } from "./config/db.js";
 import routes from "./routes/index.js";
 
-dotenv.config(); // Load environment variables
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,14 +13,12 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api", routes);
 
-// Database Connection & Sync Models
 connectDB();
-sequelize.sync() // Sync all models to the database
-  .then(() => console.log("✅ Models Synced with Database"))
-  .catch((err) => console.error("❌ Error Syncing Models:", err));
+sequelize.sync() 
+  .then(() => console.log("Models Synced with Database"))
+  .catch((err) => console.error("Error Syncing Models:", err));
 
 
 app.get("/", (req, res) => {
